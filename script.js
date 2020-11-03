309f5d4edc6bdb1cba612dce818fc66e68955b05
Daniel <43968979+DANSKI423319@users.noreply.github.com>
2020-11-03 15:59:20 +0000

    Commit 2
    Added second page and a link from the first. Place holder button added with a test function... up and running, locally

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// Java Script code goes here
+$(document).ready(function() {
+
+    var repeatingNum = 0;
+
+    $("#btnCheck").click(function() {
+        repeatingNum++;
+        console.log("Hello world! " + repeatingNum);
+    });
+
+});
