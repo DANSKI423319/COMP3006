38dd1137e7ff4eb13f91b699efecec67b0cbe5c3
Daniel <43968979+DANSKI423319@users.noreply.github.com>
2020-11-04 00:09:23 +0000

    Commit 4
    Added minimal functionality to add a room as a button, future use intended to click on a button which then loads the data to a specific room

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    var repeatingNum = 0;
+    var roomID = 0;
+    var roomArray = new Array();
 
-    $("#btnCheck").click(function() {
-        repeatingNum++;
-        console.log("Hello world! " + repeatingNum);
+    // Add a room
+    $("#btnCheck").click(function () {
+        roomID++;
+        let newRoomName = $("#txtRoom").val();
+
+        if (newRoomName == "") {
+            newRoomName = RandomNum();
+        } else {
+            roomArray.push(newRoomName);
+            console.log(roomArray.length);
+        }
+
+        $("#listBtns").append("<button id='btn" + roomID + "' class='btn btn-info'"
+            + "style='margin: 3px;'>" + newRoomName + "</button>");
     });
+
+    // Random number generator
+    function RandomNum() {
+        var RandomNum = Math.random();
+        var RefinedNum = Math.round(RandomNum * 10000);
+        return RefinedNum;
+    }
 
 });
