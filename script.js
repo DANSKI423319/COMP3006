81157c73a01368dcdf39f60da174fe4034ad7dd1
Daniel <43968979+DANSKI423319@users.noreply.github.com>
2020-11-11 12:55:40 +0000

    Commit 6
    Users can now add computers to rooms, and can see this reflected in a table
    
    Added more comments to the HTML page

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 $(document).ready(function () {
 
-    var roomID;
-    var roomArray = new Array();
+    let roomArray = new Array();
+    let computerArray = new Array();
+    let roomSel;
 
     // Add a room
     $("#btnAddRoom").click(function () {
-        roomID++;
+        let roomID = computerArray.length;
         let roomName = $("#txtRoom").val();
-        var invalid = false;
+        let invalid = false;
 
         if (roomName == "") {
             roomName = RandomNum();
-            roomArray.push(roomName);
             addNewRoom(roomID, roomName);
             $("#txtRoom").val("");
-
         } else {
-
             // Valid entry check
             for (i = 0; i < roomArray.length; i++) {
-                if (roomArray[i] == roomName) {
+                if (roomArray[i].name == roomName) {
                     invalid = true;
                     $("#txtRoom").val("");
                     { break }
@@ -28,25 +26,42 @@ $(document).ready(function () {
 
             // Valid entry
             if (invalid == false) {
-                roomArray.push(roomName);
                 addNewRoom(roomID, roomName);
                 $("#txtRoom").val("");
             }
         }
     });
 
-    // Room selection identifier
+    // Add a computer
+    $("#btnAddComputer").click(function () {
+        let tempID = $("#comInputID").val();
+        let tempStatus = $("#comInputStatus").val();
+        let tempNotes = $("#comInputNotes").val();
+        let tempRoomID = roomSel;
+        
+        if (tempID == "") { tempID = RandomNum(); }
+
+        // Add a check loop for duplicate IDs here... 
+        // ...
+        
+        addNewPC(tempID, tempStatus, tempNotes, tempRoomID);
+        
+        // Populate table
+        loadTable();
+    });
+
+    // Room selection 
     $(document).on("click", ".btn-info", function () {
         let txtTemp = ($(this).text());
-        indexTemp = returnRoomIndex(txtTemp);
+        roomSel = returnRoomIndex(txtTemp);
+        $("#btnAddComputer").prop("disabled", false);
 
-        // Tell user if something is wrong
-        if (indexTemp == "undefined") {
-            indexTemp = "ERROR";
-        }
+        // Tell user which room is selected
+        $("#txtRoomSel1").html("Selected: " + txtTemp + ", Index No. " + roomSel);
+        $("#txtRoomSel2").html("Selected: " + txtTemp + ", Index No. " + roomSel);
 
-        $("#txtRoomSel1").html("Selected: " + txtTemp + ", Index No. " + indexTemp);
-        $("#txtRoomSel2").html("Selected: " + txtTemp + ", Index No. " + indexTemp);
+        // Populate table
+        loadTable();
     });
 
     // Random number generator
@@ -58,16 +73,60 @@ $(document).ready(function () {
 
     // Add new room
     function addNewRoom(id, name) {
+        var newRoom = {
+            id: id,
+            name: name,
+        }
+
+        roomArray.push(newRoom);
         $("#listBtns").append("<button id=\"btn" + id + "\" class=\"btn-info\" style=\"margin: 3px;\">" + name + "</button>");
+    }
+
+    // Add new computer
+    function addNewPC(id, status, notes, roomid) {
+        var newPC = {
+            id: id,
+            status: status,
+            notes: notes,
+            roomid: roomid,
+        }
+
+        computerArray.push(newPC);
+        console.log(computerArray);
     }
 
     // Get index of room
     function returnRoomIndex(name) {
         for (var i = 0; i < roomArray.length; i++) {
-            if (name == roomArray[i]) {
+            if (name == roomArray[i].name) {
                 return i;
             }
         }
     }
 
+    // Load table
+    function loadTable(){
+        $("#tblComputer").empty();
+        for (i = 0; i < computerArray.length; i++) {
+            if (computerArray[i].roomid == roomSel) {
+                addToTable(computerArray[i].id, computerArray[i].status, computerArray[i].notes)
+            }
+        }
+    }
+
+    // Add to a table
+    function addToTable(entry1, entry2, entry3) {
+        $("#tblComputer").append("<tr><td>" + entry1 + "</td><td>"
+            + entry2 + "</td><td>" + entry3 + "</td></tr>");
+    }
+
+    // Code test environment
+    $(window).ready(function () {
+
+        let i = 0
+        while (i < 3) {
+            addNewRoom((i), ("Room " + i + ""));
+            i++
+        }
+    });
 });
