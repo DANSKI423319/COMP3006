1016cb9282b17a44328b0f8af1f63d672d102f14
Daniel <43968979+DANSKI423319@users.noreply.github.com>
2020-11-13 14:54:18 +0000

    Quicksave 01
    Changing script.js formation, trying to impliment tests

--- script.js
+++ script.js
@@ -1,8 +1,64 @@
-$(document).ready(function () {
+var roomArray = new Array();
+var computerArray = new Array();
+var roomSel;
 
-    let roomArray = new Array();
-    let computerArray = new Array();
-    let roomSel;
+// Random number generator
+function RandomNum() {
+    var RandomNum = Math.random();
+    var RefinedNum = Math.round(RandomNum * 10000);
+    return RefinedNum;
+}
+
+// Add new room
+function addNewRoom(id, name) {
+    var newRoom = {
+        id: id,
+        name: name,
+    }
+
+    roomArray.push(newRoom);
+    $("#listBtns").append("<button id=\"btn" + id + "\" class=\"btn-info\" style=\"margin: 3px;\">" + name + "</button>");
+}
+
+// Add new computer
+function addNewPC(id, status, notes, roomid) {
+    var newPC = {
+        id: id,
+        status: status,
+        notes: notes,
+        roomid: roomid,
+    }
+
+    computerArray.push(newPC);
+    console.log(computerArray);
+}
+
+// Get index of room
+function returnRoomIndex(name) {
+    for (var i = 0; i < roomArray.length; i++) {
+        if (name == roomArray[i].name) {
+            return i;
+        }
+    }
+}
+
+// Load table
+function loadTable(){
+    $("#tblComputer").empty();
+    for (i = 0; i < computerArray.length; i++) {
+        if (computerArray[i].roomid == roomSel) {
+            addToTable(computerArray[i].id, computerArray[i].status, computerArray[i].notes)
+        }
+    }
+}
+
+// Add to a table
+function addToTable(entry1, entry2, entry3) {
+    $("#tblComputer").append("<tr><td>" + entry1 + "</td><td>"
+        + entry2 + "</td><td>" + entry3 + "</td></tr>");
+}
+
+$(document).ready(function () {
 
     // Add a room
     $("#btnAddRoom").click(function () {
@@ -63,62 +119,6 @@ $(document).ready(function () {
         // Populate table
         loadTable();
     });
-
-    // Random number generator
-    function RandomNum() {
-        var RandomNum = Math.random();
-        var RefinedNum = Math.round(RandomNum * 10000);
-        return RefinedNum;
-    }
-
-    // Add new room
-    function addNewRoom(id, name) {
-        var newRoom = {
-            id: id,
-            name: name,
-        }
-
-        roomArray.push(newRoom);
-        $("#listBtns").append("<button id=\"btn" + id + "\" class=\"btn-info\" style=\"margin: 3px;\">" + name + "</button>");
-    }
-
-    // Add new computer
-    function addNewPC(id, status, notes, roomid) {
-        var newPC = {
-            id: id,
-            status: status,
-            notes: notes,
-            roomid: roomid,
-        }
-
-        computerArray.push(newPC);
-        console.log(computerArray);
-    }
-
-    // Get index of room
-    function returnRoomIndex(name) {
-        for (var i = 0; i < roomArray.length; i++) {
-            if (name == roomArray[i].name) {
-                return i;
-            }
-        }
-    }
-
-    // Load table
-    function loadTable(){
-        $("#tblComputer").empty();
-        for (i = 0; i < computerArray.length; i++) {
-            if (computerArray[i].roomid == roomSel) {
-                addToTable(computerArray[i].id, computerArray[i].status, computerArray[i].notes)
-            }
-        }
-    }
-
-    // Add to a table
-    function addToTable(entry1, entry2, entry3) {
-        $("#tblComputer").append("<tr><td>" + entry1 + "</td><td>"
-            + entry2 + "</td><td>" + entry3 + "</td></tr>");
-    }
 
     // Code test environment
     $(window).ready(function () {
