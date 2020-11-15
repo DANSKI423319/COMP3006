0fbc090e1df97f9975e758c2cc2f407a9ef941b4
Daniel <43968979+DANSKI423319@users.noreply.github.com>
2020-11-15 13:39:55 +0000

    Quicksave 05
    Primitive but kind fo working removal function for the table and array, has a logic error that doesn't allow the final computer to be removed
    
    Going to try and work around this by making a temporary array which can be seen as having only 1 entry when wanting to remove the final object...

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function addNewRoom(id, name) {
     }
 
     roomArray.push(newRoom);
-    $("#roomList").append("<button id=\"btn" + id + "\" class=\"btn-info\" style=\"margin: 3px;\">" + name + "</button>");
+    $('#roomList').append('<button id=\'btn' + id + '\' class=\'btn-info\' style=\'margin: 3px;\'>' + name + '</button>');
 }
 
 // Add new computer
@@ -45,7 +45,7 @@ function returnRoomIndex(name) {
 
 // Load table
 function loadTable() {
-    $("#tblComputer").empty();
+    $('#tblComputer').empty();
     for (i = 0; i < computerArray.length; i++) {
         if (computerArray[i].roomid == roomSel) {
             addToTable(computerArray[i].id, computerArray[i].status, computerArray[i].notes)
@@ -55,17 +55,17 @@ function loadTable() {
 
 // Add to a table
 function addToTable(id, status, notes) {
-    $("#tblComputer").append("<tr><td>" + id + "</td><td>"
-        + status + "</td><td>" + notes + "</td><td>"
-        + "<button>Edit</button>" + "</td></tr>");
+    $('#tblComputer').append('<tr id=' + id + '><td>' + id + '</td><td>' + status + '</td><td>' + notes + '</td><td>'
+        + '<button class=\'btn btn-warning\'>Edit</button>'
+        + '<button  class=\'btn btn-danger\'>Remove</button>' + '</td></tr>');
 }
 
 // Click event for testing
 function clickElement(element) {
     try {
-        element.trigger("click");
+        element.trigger('click');
     } catch (err) {
-        var event = new MouseEvent("click", { view: window, cancelable: true, bubbles: true });
+        var event = new MouseEvent('click', { view: window, cancelable: true, bubbles: true });
         element.dispatchEvent(event);
     }
 }
@@ -75,120 +75,143 @@ function clickElement(element) {
 $(document).ready(function () {
 
     // Add a room
-    $("#btnAddRoom").click(function () {
-        let roomID = computerArray.length;
-        let roomName = $("#txtRoom").val();
+    $('#btnAddRoom').click(function () {
+        let roomName = $('#txtRoom').val();
+        let roomID;
         let invalid = false;
 
-        if (roomName == "") {
+        if (roomName == '') {
             roomName = RandomNum();
+            roomID = roomName;
             addNewRoom(roomID, roomName);
-            $("#txtRoom").val("");
+            $('#txtRoom').val('');
         } else {
-            // Valid entry check
+            // Validity check
             for (i = 0; i < roomArray.length; i++) {
                 if (roomArray[i].name == roomName) {
                     invalid = true;
-                    $("#txtRoom").val("");
+                    $('#txtRoom').val('');
                     { break }
                 }
             }
-            // Valid entry
             if (invalid == false) {
+                roomID = RandomNum();
                 addNewRoom(roomID, roomName);
-                $("#txtRoom").val("");
+                $('#txtRoom').val('');
             }
         }
     });
 
     // Add a computer
-    $("#btnAddComputer").click(function () {
-        let tempID = $("#comInputID").val();
-        let tempStatus = $("#comInputStatus").val();
-        let tempNotes = $("#comInputNotes").val();
+    $('#btnAddComputer').click(function () {
+        let tempID = $('#comInputID').val();
+        let tempStatus = $('#comInputStatus').val();
+        let tempNotes = $('#comInputNotes').val();
         let tempRoomID = roomSel;
         let invalid = false;
 
-        if (tempID == "") {
+        if (tempID == '') {
             tempID = RandomNum();
             addNewPC(tempID, tempStatus, tempNotes, tempRoomID);
-            $("#comInputID").val("");
+            $('#comInputID').val('');
         } else {
-            // Valid entry check
+            // Validity check
             for (i = 0; i < computerArray.length; i++) {
                 if (computerArray[i].id == tempID) {
                     invalid = true;
-                    $("#comInputID").val("");
+                    $('#comInputID').val('');
                     { break }
                 }
             }
-            // Valid entry
             if (invalid == false) {
                 addNewPC(tempID, tempStatus, tempNotes, tempRoomID);
-                $("#comInputID").val("");
+                $('#comInputID').val('');
             }
         }
 
         // Reload table
         loadTable();
+        console.log("### ADDING COMPUTER")
+        console.log(computerArray)
     });
 
     // Room selection 
-    $(document).on("click", ".btn-info", function () {
+    $(document).on('click', '.btn-info', function () {
         let txtTemp = ($(this).text());
         roomSel = returnRoomIndex(txtTemp);
-        $("#btnAddComputer").prop("disabled", false);
+        $('#btnAddComputer').prop('disabled', false);
 
         // Tell user which room is selected
-        $("#txtRoomSel1").html("Selected: " + txtTemp + ", Index No. " + roomSel);
-        $("#txtRoomSel2").html("Selected: " + txtTemp + ", Index No. " + roomSel);
+        $('#txtRoomSel1').html('Selected: ' + txtTemp + ', Index No. ' + roomSel);
+        $('#txtRoomSel2').html('Selected: ' + txtTemp + ', Index No. ' + roomSel);
 
         // Populate table
         loadTable();
     });
 
-    // $(window).ready(function () {} );
+    // Computer removal
+    $(document).on('click', '.btn-danger', function() {
+        //let txtTemp = '#' + ($(this).attr('id'));
+        //alert(txtTemp);
+        let col = ($(this).closest('tr').find('td:eq(0)').text());
+        let row = "#" + ($(this).closest('tr').attr('id'));
+
+        console.log(col);
+
+        for (i = 0; i < computerArray.length; i++) {
+            if (computerArray[i].id == col) {
+                computerArray.splice(i, i);
+                { break }
+            }
+        }
+
+        $(row).remove();      
+        console.log(computerArray)         
+    });
+
+
+    // $(window).ready(function () {});
 
 });
 
 /////////////////////// MOCHA & CHAI TESTING ///////////////////////
 
-suite("Testing Suite", function () {
+suite('Testing Suite', function () {
 
     setup(function () {
-        this.roomName = "Room 4";
+        this.roomName = 'Room 4';
         this.roomID = computerArray.length;
-        this.pcID = "D29PC";
-        this.pcStatus = "Working";
-        this.notes = "N/a"
+        this.pcID = 'D29PC';
+        this.pcStatus = 'Working';
+        this.notes = 'N/a'
         this.pcRoomID = 0;
     });
 
     teardown(function () {
-        $("#roomList").empty();
+        $('#roomList').empty();
         roomArray = [];
-        $("#tblComputer").empty();
+        $('#tblComputer').empty();
         computerArray = [];
     });
 
-    test("Add a room", function () {
+    test('Add a room', function () {
         addNewRoom(this.roomID, this.roomName);
 
         /*
         - Test UI, inoperable -
-        $("txtRoom").val(this.roomName); 
-        clickElement($("#btnAddRoom"));
+        $('txtRoom').val(this.roomName); 
+        clickElement($('#btnAddRoom'));
         */
 
-        let checker = $("#btn0").html();
-        chai.assert.equal(this.roomName, checker, "Cannot find expected Room value");
+        let checker = $('#btn0').html();
+        chai.assert.equal(this.roomName, checker, 'Cannot find expected Room value');
     });
 
-    test("Add a computer", function () {
+    test('Add a computer', function () {
         addNewRoom(this.roomID, this.roomName);
         addNewPC(this.pcID, this.pcStatus, this.pcNotes, this.pcRoomID);
 
         let checker = computerArray[0].id;
-        chai.assert.equal(this.pcID, checker, "Cannot find expected PC ID");
+        chai.assert.equal(this.pcID, checker, 'Cannot find expected PC ID');
     });
 });
